c7c61c4117195fc7a9ee1ce958552869610a85fd
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-07-18 19:39:51 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt")
     return config
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
+
