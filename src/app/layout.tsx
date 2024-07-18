a33ac4b671cebf2fa87fd6a58452ae97d9fbeb62
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-07-18 19:55:34 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/sonner"
-import { NavBar } from "@/components/nav"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,7 +26,6 @@ export default function RootLayout({
           position="top-center"
           richColors
         />
-        <NavBar />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { buttonVariants } from "@/components/ui/button"
 import { Features } from "@/components/features"
 import { Footer } from "@/components/footer"
 import Link from "next/link"
+import { NavBar } from "@/components/nav"
 
 const page = () => {
   const divStyle = {
@@ -15,37 +16,40 @@ const page = () => {
   }
 
   return (
-    <div style={divStyle}>
-      <div className="dark:text-zinc-100 mx-auto w-7/8 ">
-        <div className="w-7/8 mx-auto relative h-4/5 z-30 pt-6 inter  ">
-          <div className=" text-center pt-8 md:pt-12 md:mt-12 mx-auto w-4/5 md:w-3/5">
-            <h1 className="text-[#030647]  font-semibold text-5xl md:text-7xl dark:text-gray-200 Inter">
-              {" "}
-              The All-In-One Auth Solution for SaaS Founders{" "}
-            </h1>
-            <p className="text-lg text-[#515568] pt-2 mt-4 text-center w-4/5  mx-auto dark:text-gray-100/70 Inter">
-              Streamline your workflow with the all-in-one calendar and organization app for
-              founders. Get started with a free trial today, no credit card needed.{""}
-            </p>
-            <div className="flex flex-row mx-auto justify-center pt-6 mt-4">
-              <Link
-                href="/auth"
-                className={buttonVariants({
-                  size: "default",
-                  className:
-                    "md:py-6 md:text-medium focus:ring-4 font-semibold rounded-xl px-10 py-4 mb-2",
-                })}
-              >
-                Get Started
-              </Link>
+    <>
+      <NavBar />
+      <div style={divStyle}>
+        <div className="dark:text-zinc-100 mx-auto w-7/8 ">
+          <div className="w-7/8 mx-auto relative h-4/5 z-30 pt-6 inter  ">
+            <div className=" text-center pt-8 md:pt-12 md:mt-12 mx-auto w-4/5 md:w-3/5">
+              <h1 className="text-[#030647]  font-semibold text-5xl md:text-7xl dark:text-gray-200 Inter">
+                {" "}
+                The All-In-One Auth Solution for SaaS Founders{" "}
+              </h1>
+              <p className="text-lg text-[#515568] pt-2 mt-4 text-center w-4/5  mx-auto dark:text-gray-100/70 Inter">
+                Streamline your workflow with the all-in-one Auth Solution for organization app for
+                founders. Get started with a free trial today, no credit card needed.{""}
+              </p>
+              <div className="flex flex-row mx-auto justify-center pt-6 mt-4">
+                <Link
+                  href="/auth"
+                  className={buttonVariants({
+                    size: "default",
+                    className:
+                      "md:py-6 md:text-medium focus:ring-4 font-semibold rounded-xl px-10 py-4 mb-2",
+                  })}
+                >
+                  Get Started
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
 
-        <Features />
-        <Footer />
+          <Features />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
